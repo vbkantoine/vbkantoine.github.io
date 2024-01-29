ea6e9c032dce50a372e3bf3087f81e697b10822e
vbkantoine <antoine.vanb@aol.fr>
2024-01-29 16:15:23 +0100

    up

--- js/sort_publications.js
+++ js/sort_publications.js
@@ -8,67 +8,68 @@ function extractReferencesFromBibtex(bibtex) {
     var entries = bibtex.split('\n@');
 
     // Parcourir chaque entrée et extraire les informations nécessaires
-    for (var i = 1; i < entries.length; i++) {
+    for (var i = 0; i < entries.length; i++) {
         // Extraire le type d'entrée (article, book, etc.)
-        var entryType = entries[i].substring(0, entries[i].indexOf('{')).trim().toLowerCase();
+        if (entries[i].match('{')) {
+            var entryType = entries[i].substring(0, entries[i].indexOf('{')).trim().toLowerCase();
 
-        // Extraire les champs (author, title, year) de chaque entrée
-        var authorMatch = entries[i].match(/author\s*=\s*{([^}]*)}/);
-        var titleMatch = entries[i].match(/title\s*=\s*{([^}]*)}/);
-        var yearMatch = entries[i].match(/year\s*=\s*{([^}]*)}/);
-        var journalMatch = entries[i].match(/journal\s*=\s*{([^}]*)}/);
-        var volumeMatch = entries[i].match(/volume\s*=\s*{([^}]*)}/);
-        var numberMatch = entries[i].match(/number\s*=\s*{([^}]*)}/);
-        var doiMatch = entries[i].match(/doi\s*=\s*{([^}]*)}/);
-        var urlMatch = entries[i].match(/url\s*=\s*{([^}]*)}/);
-        var monthMatch = entries[i].match(/month\s*=\s*{([^}]*)}/);
+            // Extraire les champs (author, title, year) de chaque entrée
+            var authorMatch = entries[i].match(/author\s*=\s*{([^}]*)}/);
+            var titleMatch = entries[i].match(/title\s*=\s*{([^}]*)}/);
+            var yearMatch = entries[i].match(/year\s*=\s*{([^}]*)}/);
+            var journalMatch = entries[i].match(/journal\s*=\s*{([^}]*)}/);
+            var volumeMatch = entries[i].match(/volume\s*=\s*{([^}]*)}/);
+            var numberMatch = entries[i].match(/number\s*=\s*{([^}]*)}/);
+            var doiMatch = entries[i].match(/doi\s*=\s*{([^}]*)}/);
+            var urlMatch = entries[i].match(/url\s*=\s*{([^}]*)}/);
+            var monthMatch = entries[i].match(/month\s*=\s*{([^}]*)}/);
 
-        // Créer un dictionnaire pour stocker les informations de référence
-        var reference = {};
+            // Créer un dictionnaire pour stocker les informations de référence
+            var reference = {};
 
-        reference.type = entryType;
+            reference.type = entryType;
 
-        // Remplir le dictionnaire avec les informations extraites
-        if (authorMatch) {
-            reference.authors = authorMatch[1].split(' and ').map(function(author) {
-                return author.trim();
-            });
-        }
-
-        if (titleMatch) {
-            reference.title = titleMatch[1].trim();
-        }
-        if (yearMatch) {
-            reference.year = parseInt(yearMatch[1].trim(), 10);
-        }
-        if (journalMatch) {
-            reference.journal = journalMatch[1].trim();
-        }
-        if (volumeMatch) {
-            reference.volume = parseInt(volumeMatch[1].trim(), 10);
-        }
-        if (numberMatch) {
-            reference.number = parseInt(numberMatch[1].trim(), 10);
-        }
-        if (doiMatch) {
-            reference.doi = doiMatch[1].trim();
-        }
-        if (urlMatch) {
-            reference.url = urlMatch[1].trim();
-        }
-        if (monthMatch) {
-            reference.month = parseInt(monthMatch[1].trim(), 10);
-        } else {
-            reference.month = 0;
-        }
-
-        // Ajouter la référence au tableau
-        for (const author of reference.authors) {
-            if (author.toLowerCase().match('biesbroeck')) {
-            // if (true) {
-                references.push(reference);
+            // Remplir le dictionnaire avec les informations extraites
+            if (authorMatch) {
+                reference.authors = authorMatch[1].split(' and ').map(function(author) {
+                    return author.trim();
+                });
             }
-        
+
+            if (titleMatch) {
+                reference.title = titleMatch[1].trim();
+            }
+            if (yearMatch) {
+                reference.year = parseInt(yearMatch[1].trim(), 10);
+            }
+            if (journalMatch) {
+                reference.journal = journalMatch[1].trim();
+            }
+            if (volumeMatch) {
+                reference.volume = parseInt(volumeMatch[1].trim(), 10);
+            }
+            if (numberMatch) {
+                reference.number = parseInt(numberMatch[1].trim(), 10);
+            }
+            if (doiMatch) {
+                reference.doi = doiMatch[1].trim();
+            }
+            if (urlMatch) {
+                reference.url = urlMatch[1].trim();
+            }
+            if (monthMatch) {
+                reference.month = parseInt(monthMatch[1].trim(), 10);
+            } else {
+                reference.month = 0;
+            }
+
+            // Ajouter la référence au tableau
+            for (const author of reference.authors) {
+                if (author.toLowerCase().match('biesbroeck')) {
+                // if (true) {
+                    references.push(reference);
+                }
+            }
         }
     }
 
