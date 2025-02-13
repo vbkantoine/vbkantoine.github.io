9dc5952719676db0c5b4a380a5d8eb507a0d812a
vbkantoine <antoine.vanb@aol.fr>
2025-02-13 20:15:08 +0100

    update links

--- js/common.js
+++ js/common.js
@@ -1,9 +1,26 @@
-
+function readTextFile2(file) {
+  var rawFile = new XMLHttpRequest();
+  var allText = ''
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if(rawFile.readyState === 4)  {
+      if(rawFile.status === 200 || rawFile.status == 0) {
+        allText = rawFile.responseText;
+        console.log(allText);
+       }
+    }
+  }
+  //if (rawFile.responseText) {
+  // var allText = rawFile.responseText;
+  rawFile.send(null);
+  return allText;
+  //}
+}
 
 document.addEventListener('DOMContentLoaded',function(){
-    var headdiv = readTextFile('./common/headdiv.html');
+    var headdiv = readTextFile2('./common/headdiv.html');
     document.getElementsByClassName("container").innerHTML = headdiv;
-    var header = readTextFile('./common/header.html');
+    var header = readTextFile2('./common/header.html');
     document.getElementsByClassName("header").innerHTML = header;
     // var foot
 });
